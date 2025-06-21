4ec17593b79b756cd34f497d2997b4e1a5065617
Ahmad Uffi <144457661+ahmadUffi@users.noreply.github.com>
2025-06-21 20:25:18 +0700

    Update page.jsx

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -22,7 +22,7 @@ export default function HomePage() {
       >
         <PilihanBelajar />
         <Province />
-        {/* <OverviewIndonesia /> */}
+        <OverviewIndonesia /> 
       </div>
       <Footer />
       <FloatChat />
