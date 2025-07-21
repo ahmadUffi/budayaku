d73619fb3753efa73a76bad156d5077adfd8868a
ahmadUffi <ahmaduffi45@gmail.com>
2025-07-21 21:47:13 +0700

    Remove commented-out audio playback button code in ChatMessage component

--- components/ChatMessage.jsx
+++ components/ChatMessage.jsx
@@ -33,40 +33,6 @@ const ChatMessage = ({
           ) : (
             <p className="flex-1">{message}</p>
           )}
-
-          {/* {!isUser && onPlayAudio && (
-            <button
-              onClick={handlePlayAudio}
-              disabled={isLoadingAudio}
-              className={`ml-1 p-1 rounded-full transition-colors flex-shrink-0
-                ${
-                  isPlayingAudio
-                    ? "bg-red-500 hover:bg-red-600 text-white"
-                    : "bg-gray-300 hover:bg-gray-400 text-gray-700"
-                }`}
-              title={isPlayingAudio ? "Stop audio" : "Play audio"}
-            >
-              {isLoadingAudio ? (
-                <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-current"></div>
-              ) : isPlayingAudio ? (
-                <svg
-                  className="w-4 h-4"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M6 4h4v16H6V4zm8 0h4v16h-4V4z" />
-                </svg>
-              ) : (
-                <svg
-                  className="w-4 h-4"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8 5v14l11-7z" />
-                </svg>
-              )}
-            </button>
-          )} */}
         </div>
       </div>
     </div>
